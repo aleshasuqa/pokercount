4c33c571f89caa0af8d7a0fa41dece0617a8e8ea
Aleksei <aleksei.kotliarov@epita.fr>
2025-03-02 16:35:37 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="text-3xl font-bold underline">
+        Hello world! Count: {count}
+      </p>
+    </>
+  )
+}
+
+export default App
